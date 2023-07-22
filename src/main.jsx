a45cc781ac2ed6f358acce45541674949e0d26fb
Md-arefin <ansariarefin617@gmail.com>
2023-07-23 01:03:04 +0600

    added header

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {createBrowserRouter,RouterProvider,} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, } from "react-router-dom";
 import Main from './Layout/Main.jsx';
 import Home from './Pages/Home/Home.jsx';
 import Login from './Pages/Login/Login.jsx';
@@ -11,27 +11,29 @@ import SignUp from './Pages/SignUp/SignUp.jsx';
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Main/>,
-    errorElement: <App/>,
+    element: <Main />,
+    errorElement: <App />,
     children: [
       {
         path: "/",
-        element: <Home/>,
+        element: <Home />,
       },
     ]
   },
   {
     path: '/login',
-    element: <Login/>
+    element: <Login />
   },
   {
     path: '/signUp',
-    element: <SignUp/>
+    element: <SignUp />
   }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <RouterProvider router={router} />
+    <div className='px-4 md:5 lg:px-8'>
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>,
 )
