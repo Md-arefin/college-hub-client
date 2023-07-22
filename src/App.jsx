bb5a1aff1ececf7785915d698c71df82c91c7c76
Md-arefin <ansariarefin617@gmail.com>
2023-07-23 00:45:03 +0600

    added signUp page

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
+import React from 'react';
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
+import { Link } from 'react-router-dom';
 
 
-function App() {
+const ErrorPage = () => {
+    return (
+        <div className='relative'>
+            <div>
+                <Player
+                    autoplay
+                    loop
+                    src="https://assets4.lottiefiles.com/private_files/lf30_bfxeb0to.json"
+                    className='xl:w-[1000px] lg:w-[800px] md:w-[700px] sm:w-[450px]
+                 w-[350px]'
+                >
+                    <Controls visible={!true} buttons={['play', 'repeat', 'frame', 'debug']} />
+                </Player>
+            </div>
+            <Link to='/'>
+                <div className='text-center'>
+                    <button className="btn bg-rose-700 hover:text-black border-b-4 border-0 border-black  text-white ">Back to Home</button>
+                </div>
+            </Link>
+        </div>
+    );
+};
 
-
-  return (
-    <>
-      <div>
-        
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-      
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default ErrorPage;
