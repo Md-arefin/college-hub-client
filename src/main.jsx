a047fbf9a70c277acbc32c0be7b2259acfadf2a5
Md-arefin <ansariarefin617@gmail.com>
2023-07-23 00:38:18 +0600

    added login page

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import {createBrowserRouter,RouterProvider,} from "react-router-dom";
+import Main from './Layout/Main.jsx';
+import Home from './Pages/Home/Home.jsx';
+import Login from './Pages/Login/Login.jsx';
+import SignUp from './Pages/SignUp/SignUp.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
+    element: <Main/>,
+    errorElement: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>,
+      },
+    ]
   },
+  {
+    path: '/login',
+    element: <Login/>
+  },
+  {
+    path: '/signUp',
+    element: <SignUp/>
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
