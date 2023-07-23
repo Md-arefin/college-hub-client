32d7479fa2f1aab7a71131147655277c666eb3af
Md-arefin <ansariarefin617@gmail.com>
2023-07-23 13:08:35 +0600

    added clg name

--- src/Pages/CollegePage/CollegePage.jsx
+++ src/Pages/CollegePage/CollegePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Controls, Player } from '@lottiefiles/react-lottie-player';
 
 const CollegePage = () => {
     return (
@@ -14,13 +15,32 @@ const CollegePage = () => {
                         </h3>
 
                         <p className='md:ml-10 lg:ml-10 lg:px-5 md:text-center my-5 md:max-w-[80%] lg:max-w-[75%]'>
-                            Embark on a journey of academic brilliance as you explore the captivating realm of colleges. Immerse yourself in a myriad of enticing options, each adorned with college images, ratings, admission dates, and research accomplishments. 
+                            Embark on a journey of academic brilliance as you explore the captivating realm of colleges. Immerse yourself in a myriad of enticing options, each adorned with college images, ratings, admission dates, and research accomplishments.
                             <span className='hidden md:block'>
-                            Unveil the secrets behind their events and sports facilities by delving into the enticing details, awaiting your eager curiosity. Welcome to a world of limitless possibilities and enriching experiences.
+                                Unveil the secrets behind their events and sports facilities by delving into the enticing details, awaiting your eager curiosity. Welcome to a world of limitless possibilities and enriching experiences.
                             </span>
                         </p>
                     </div>
 
+                </div>
+            </div>
+            {/* college name section */}
+
+            <h3 className='text-center my-16 text-lg md:text-3xl lg:text-5xl font-semibold'>
+            Gateway to Knowledge: College Directory
+            </h3>
+
+            <div className='flex flex-col md:flex-row gap-5 w-[full] border-4 my-16 px-16'>
+
+                <div>
+                    <Player
+                        autoplay
+                        loop
+                        src="https://lottie.host/961db871-fa76-4922-b451-19d882fa7508/dUiYdFRHyH.json"
+                        className='w-[75%]'
+                    >
+                        <Controls visible={!true} buttons={['play', 'repeat', 'frame', 'debug']} />
+                    </Player>
                 </div>
             </div>
         </div>
