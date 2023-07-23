696b362f2e00fa5cc1862feb922c0ea03a6a433a
Md-arefin <ansariarefin617@gmail.com>
2023-07-23 13:47:51 +0600

    added clg name in tbl

--- src/Pages/CollegePage/CollegePage.jsx
+++ src/Pages/CollegePage/CollegePage.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Controls, Player } from '@lottiefiles/react-lottie-player';
 
 const CollegePage = () => {
+
+    const [colleges, setColleges] = useState([]);
+
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setColleges(data)
+            })
+    }, [])
+
+    console.log(colleges)
+
     return (
         <div className=''>
             <div className="relative bg-[url('/images/College-banner.jpg')] bg-cover bg-no-repeat h-[60vh] lg:h-[80vh] w-full ">
@@ -26,24 +40,50 @@ const CollegePage = () => {
             </div>
             {/* college name section */}
 
-            <h3 className='text-center my-16 text-lg md:text-3xl lg:text-5xl font-semibold'>
-            Gateway to Knowledge: College Directory
+            <h3 className='text-center my-16 text-2xl md:text-3xl lg:text-5xl font-semibold'>
+                Gateway to Knowledge: College Directory
             </h3>
 
-            <div className='flex flex-col md:flex-row gap-5 w-[full] border-4 my-16 px-16'>
+            <div className='flex flex-col md:flex-row gap-5 w-[full] justify-center my-16 lg:px-16'>
 
                 <div>
                     <Player
                         autoplay
                         loop
                         src="https://lottie.host/961db871-fa76-4922-b451-19d882fa7508/dUiYdFRHyH.json"
-                        className='w-[75%]'
+                        className='w-[100%]'
                     >
                         <Controls visible={!true} buttons={['play', 'repeat', 'frame', 'debug']} />
                     </Player>
                 </div>
+
+                {/* table */}
+
+                <div className="overflow-x-auto w-full md:w-[50%] border-2 rounded-md">
+                    <table className="table table-zebra">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th className='font-semibold text-black text-lg text-center'>Name</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            {
+                                colleges.map((clg, i) =>
+                                    < tr key={i} >
+                                        <th className='text-center'>{i + 1}</th>
+                                        <td className='text-lg'>{clg.collegeName}</td>
+                                    </tr>
+                                )
+                            }
+                        </tbody>
+                    </table>
+
+                </div>
+
             </div>
-        </div>
+        </div >
     );
 };
 
