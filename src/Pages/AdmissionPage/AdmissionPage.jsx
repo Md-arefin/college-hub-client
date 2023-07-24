1a301b0d7dbb6358063766a8ce94fec7795a2ada
Md-arefin <ansariarefin617@gmail.com>
2023-07-24 20:12:54 +0600

    added matha

--- src/Pages/AdmissionPage/AdmissionPage.jsx
+++ src/Pages/AdmissionPage/AdmissionPage.jsx
@@ -3,9 +3,20 @@ import React, { useEffect, useState } from 'react';
 const AdmissionPage = () => {
 
     const [colleges, setColleges] = useState([]);
+    const [showDetails, setShowDetails] = useState(false);
+
+
+
+    const handleDetailsClick = (collegeId) => {
+        const id = collegeId;
+
+        setShowDetails(true);
+
+
+    };
 
     useEffect(() => {
-        fetch('data.json')
+        fetch('http://localhost:5000/colleges-name')
             .then(res => res.json())
             .then(data => {
                 console.log(data)
@@ -50,7 +61,7 @@ const AdmissionPage = () => {
                                 colleges.map((clg, i) =>
                                     < tr key={i} >
                                         <th className='text-center'>{i + 1}</th>
-                                        <td className='text-lg cursor-pointer'>{clg.collegeName}</td>
+                                        <td onClick={() => handleDetailsClick(clg._id)} className='text-lg cursor-pointer'>{clg.collegeName}</td>
                                     </tr>
                                 )
                             }
@@ -70,66 +81,71 @@ const AdmissionPage = () => {
 
             <div className='mx-auto md:w-1/2'>
 
-                <h3 className='text-center my-16 text-2xl md:text-3xl lg:text-5xl font-semibold'>
-                    Admission Application
-                </h3>
 
-                <div className="hero-content">
+                {showDetails && <>
+                    <h3 className='text-center my-16 text-2xl md:text-3xl lg:text-5xl font-semibold'>
+                        Admission Application
+                    </h3>
 
-                    <div className="card w-full shadow-2xl bg-rose-900">
-                        <div className="card-body">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text text-white">Name</span>
-                                </label>
-                                <input type="text" placeholder="Candidate Name" className="input input-bordered" />
-                            </div>
+                    <div className="hero-content">
 
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text text-white">Subject</span>
-                                </label>
-                                <input type="text" placeholder="Subject" className="input input-bordered" />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text text-white">Email</span>
-                                </label>
-                                <input type="text" placeholder="Candidate Email" className="input input-bordered" />
-                            </div>
+                        <div className="card w-full shadow-2xl bg-rose-900">
+                            <form className="card-body">
 
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text text-white">Phone</span>
-                                </label>
-                                <input type="text" placeholder="Candidate Phone number" className="input input-bordered" />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text text-white">Address</span>
-                                </label>
-                                <input type="text" placeholder="address" className="input input-bordered" />
-                            </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text text-white">Name</span>
+                                    </label>
+                                    <input type="text" placeholder="Candidate Name" name='name' className="input input-bordered" />
+                                </div>
 
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text text-white">Date Of Birth</span>
-                                </label>
-                                <input type="text" placeholder="date of birth" className="input input-bordered" />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text text-white">Photo</span>
-                                </label>
-                                <input type="text" placeholder="photo" className="input input-bordered" />
-                            </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text text-white">Subject</span>
+                                    </label>
+                                    <input type="text" placeholder="Subject" name='subject' className="input input-bordered" />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text text-white">Email</span>
+                                    </label>
+                                    <input type="text" placeholder="Candidate Email" name='email' className="input input-bordered" />
+                                </div>
 
-                            <div className="form-control mt-6">
-                                <button className="btn bg-black text-white hover:bg-white hover:text-black w-1/2 border-none mx-auto">Submit</button>
-                            </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text text-white">Phone</span>
+                                    </label>
+                                    <input type="text" placeholder="Candidate Phone number" name='phone' className="input input-bordered" />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text text-white">Address</span>
+                                    </label>
+                                    <input type="text" placeholder="address" name='address' className="input input-bordered" />
+                                </div>
+
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text text-white">Date Of Birth</span>
+                                    </label>
+                                    <input type="text" placeholder="date of birth" name='DOB' className="input input-bordered" />
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text text-white">Photo</span>
+                                    </label>
+                                    <input type="file" placeholder="photo" name='photo' className=" input input-bordered" />
+                                </div>
+
+                                <div className="form-control mt-6">
+                                    <input type='submit' value='Submit' className="btn bg-black text-white hover:bg-white hover:text-black w-1/2 border-none mx-auto" />
+                                </div>
+                            </form>
                         </div>
                     </div>
-                </div>
+                </>}
+
 
             </div>
 
